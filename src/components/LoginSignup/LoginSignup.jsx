d4b9939a97df3be84ef8247be3185b8465e61e2d
Sachin Alam <sachinalam4216@gmail.com>
2024-08-07 13:37:45 +0530

    UI Done

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,10 +1,13 @@
 import "./LoginSignup.css";
 
+import { useState } from "react";
 import email_icon from "../../assets/email.png";
 import password_icon from "../../assets/password.png";
 import user_icon from "../../assets/person.png";
 
 const LoginSignup = () => {
+  const [action, setAction] = useState("Sign Up");
+
   return (
     <div className="container">
       {/* Header */}
@@ -14,21 +17,30 @@ const LoginSignup = () => {
       </div>
 
       {/* Inputs */}
-      <div className="inputs"></div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name" />
+        </div>
 
-      <div className="input">
-        <img src={user_icon} alt="" />
-        <input type="text" />
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email ID" />
+        </div>
+
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password" />
+        </div>
       </div>
 
-      <div className="input">
-        <img src={email_icon} alt="" />
-        <input type="email" />
+      <div className="forget-password">
+        Lost Password? <span>Click Here</span>
       </div>
 
-      <div className="input">
-        <img src={password_icon} alt="" />
-        <input type="password" />
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
       </div>
     </div>
   );
